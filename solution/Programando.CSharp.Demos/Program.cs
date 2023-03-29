1ecbe543cdb0c031fc3141418a6115588d9aa4c8
Angel Alejandro Ruiz Centeno <aleangelrc@gmail.com>
2023-03-29 13:30:30 +0200

    Clase 29_03

--- solution/Programando.CSharp.Demos/Program.cs
+++ solution/Programando.CSharp.Demos/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Universidad;
+using System.Collections;
+using System.Collections.Generic;
 
 
 
@@ -12,30 +14,30 @@ namespace Programando.CSharp.Demos
         public static void Main(string[] args)
         {
             Console.Clear();
-            SentenciasControl();
+            //SentenciasControl();
             //SentenciasControl2();
         }
 
 
-         static void SentenciasControl()
+        static void SentenciasControl()
         {
-            Reserva[] reservas_list= new Reserva[5];
-//Modo 1 de crear reserva. Accede porque las variables son publicas
+            Reserva[] reservas_list = new Reserva[5];
+            //Modo 1 de crear reserva. Accede porque las variables son publicas
             var reserva = new Reserva();
             reserva.id = "1066HC";
             reserva.cliente = "Ana García";
             reserva.fumador = true;
-            
-            
-            
-//Modo 2 de crear reserva.Pasa los parametros entre llaves
+
+
+
+            //Modo 2 de crear reserva.Pasa los parametros entre llaves
             var reserva2 = new Reserva()
             {
                 id = "1066HC",
                 cliente = "Ana García",
                 fumador = true
             };
-//Modo 3 de crear reserva.Preguntando y rellenando los datos
+            //Modo 3 de crear reserva.Preguntando y rellenando los datos
             var reserva3 = new Reserva();
 
             Console.Write("ID Reserva: ");
@@ -43,7 +45,7 @@ namespace Programando.CSharp.Demos
 
             Console.Write("Nombre de Cliente: ");
             reserva3.cliente = Console.ReadLine();
-            
+
             // 100: Individual 200: Doble 300: Junior Suite 400: Suite
             Console.Write("Tipo de habitación: ");
             //int.TryParse(Console.ReadLine(),out reserva3.tipo); //Mejor
@@ -52,23 +54,23 @@ namespace Programando.CSharp.Demos
             switch (valor)
             {
                 case "100":
-                    reserva3.tipo=100;
+                    reserva3.tipo = 100;
                     break;
                 case "200":
-                    reserva3.tipo=200;
+                    reserva3.tipo = 200;
                     break;
                 case "300":
-                    reserva3.tipo=300;
+                    reserva3.tipo = 300;
                     break;
                 case "400":
-                    reserva3.tipo=400;
+                    reserva3.tipo = 400;
                     break;
                 default:
-                    reserva3.tipo=-1;
+                    reserva3.tipo = -1;
                     break;
             }
 
-            
+
 
             Console.Write("Es fumador? si o no: ");
             var valor2 = Console.ReadLine();
@@ -111,7 +113,212 @@ namespace Programando.CSharp.Demos
                 Console.WriteLine($"{fruta}");
             }
         }
-       
+
+
+        public static void ArrayList()
+        {
+            //Instanciar
+            ArrayList array = new ArrayList();
+
+            // Limpiar y eliminar los elementos
+            array.Clear();
+
+            // Añadir elementos
+            array.Add(1);
+            array.Add("Borja");
+            array.Add(new { Nombre = "Borja", Apellidos = "Cabeza" });
+            array.Add(new Alumno());
+
+            // Añadir elementos en una posicion
+            array.Insert(4, "blanco");
+
+            // Añadir todos los elementos de otra colección
+            var colores = new string[] { "azul", "rojo", "verde", "amarillo" };
+            array.AddRange(colores);
+
+            // Numero de elementos del ArrayList
+            Console.WriteLine(array.Count);
+
+            //Eliminar elementos
+            array.Remove("azul");
+            array.RemoveAt(4);
+            array.RemoveRange(2, 2);
+
+            // Como saber si el elemento esta contenido
+            Console.WriteLine($"Contiene el item rojo: {array.Contains("rojo")}");
+
+            // Ordena los elementos
+            array.Sort();
+
+            // Invertir el orden
+            array.Reverse();
+
+            // Convertir en un array de objetos-> object[] array = new array[10]
+            var nuevoArray = array.ToArray();
+
+            // Recorrer la colección
+            foreach (var item in array)
+            {
+                Console.WriteLine($"Item: {item.GetType().ToString()}");
+            }
+
+            for (var i = 0; i < array.Count; i++)
+            {
+                Console.WriteLine($"{i}# {array[i]}");
+            }
+
+
+        }
+
+        public static void Hashtable()//Hashtable son los diccionarios, hash es clave
+        {
+            //Instanciar
+            var ht = new Hashtable();
+
+            // Eliminar todos los elementos
+            ht.Clear();
+
+            // Añadir elementos
+            ht.Add(1200, "Borja Cabeza");
+            ht.Add("AnnaTR", "Ana Trujillop");
+            ht.Add(412, new Alumno());
+
+            // Numero de elementos
+            Console.WriteLine($"Numero de elementos: {ht.Count}");
+
+            // Eliminar un elemento
+            ht.Remove(1200);
+
+            // Recorrer el HashTable
+            foreach (var clave in ht.Keys)
+                Console.WriteLine($"{clave}: {ht[clave]}");
+
+        }
+
+        public static void List()
+        {
+
+            // Instanciar
+            List<string> list = new List<string>();
+
+            List<string> list1 = new();
+            var list2 = new List<string>();
+
+            // Eliminar los elementos
+
+            list.Clear();
+
+
+
+            // Añadir elementos
+
+            list.Add("azul");
+
+            list.Add("verde");
+
+            list.Add("rosa");
+
+            list.Add("amarillo");
+
+
+
+            // Añadir elementos en una posición
+
+            list.Insert(4, "blanco");
+
+
+
+            // Añadir todos los elementos de otra colección
+
+            var colores = new string[] { "marron", "naranja", "negro", "violeta" };
+
+            list.AddRange(colores);
+
+
+
+            // Número de elementos del List
+
+            Console.WriteLine($"Número de elementos: {list.Count}");
+
+
+
+            // Eliminar elementos
+
+            list.Remove("azul");
+
+            list.RemoveAt(4);
+
+            list.RemoveRange(2, 2);
+
+
+
+            // Saber si un elemento esta contenido
+
+            Console.WriteLine($"Contiene el item rojo: {list.Contains("rojo")}");
+
+
+
+            // Orderna los elementos
+
+            list.Sort();
+
+
+
+            // Invertir el orden
+
+            list.Reverse();
+
+
+
+            // Convertir en un array de object -> object[] array = new array[10]
+
+            var nuevoArray = list.ToArray();
+
+
+
+            // Recorrer el List
+
+            foreach (var item in list)
+
+                Console.WriteLine($"{list.IndexOf(item)}# Item: {item}");
+
+
+
+            for (var i = 0; i < list.Count; i++)
+
+                Console.WriteLine($"{i}# {list[i]}");
+        }
+
+        public static void Dictionary()
+        {
+            // Instanciar
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+            Dictionary<int, string> dic1 = new();
+            var dic2 = new Dictionary<int, string>();
+
+            // Eliminar todos los elementos
+            dic.Clear();
+
+            // Añadir elementos
+            dic.Add(1200, "Borja Cabeza");
+            dic.Add(1300, "Ana Trujillo");
+            dic.Add(1412, "José Guzman");
+
+            // Modificar un valor
+            dic[1300] = "Antonio Trujillo";
+
+            // Número de elementos
+            Console.WriteLine($"Número de elementos: {dic.Count}");
+
+            // Eliminar un elemento
+            dic.Remove(1200);
+
+            // Recorrer el diccionario
+            foreach (var clave in dic.Keys)
+                Console.WriteLine($"{clave}: {dic[clave]}");
+        }
+
     }
 }
 
---
+++ solution/Programando.CSharp.Ejercicios.ConsoleApp1/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Programando.CSharp.Ejercicios.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsultasBasicas();
+        }
+
+        static void ConsultasBasicas()
+        {
+            //T-SQL:SELECT * FROM dbo.ListaProductos
+
+            // Métodos de LINQ
+            var r1a = DataLists.ListaProductos
+                .Select(r => r);//lambda function
+
+            // Expresion de LINQ
+            var r1b = from r in DataLists.ListaProductos
+                      select r;
+
+            foreach (Producto item in r1a) Console.WriteLine($"{item.Id} {item.Descripcion}");
+
+
+
+            //T-SQL:SELECT Nombre FROM dbo.ListaClientes
+
+
+            // Métodos de LINQ
+
+            var r2a = DataLists.ListaClientes
+
+                .Select(r => r.Nombre);
+
+            // Expresión LINQ
+
+            var r2b = from r in DataLists.ListaClientes
+
+                      select r.Nombre;
+
+
+            //T-SQL:SELECT Id, Nombre FROM dbo.ListaClientes
+
+            // Métodos de LINQ
+            var r3a = DataLists.ListaClientes
+
+                .Select(r => new { r.Id, r.Nombre });
+
+            // Expresión LINQ
+            var r3b = from r in DataLists.ListaClientes
+
+                      select r.Id;
+
+            //T-SQL:SELECT Id As Code, Nombre As Nombrecompleto FROM dbo.ListaClientes
+            var r3c = DataLists.ListaClientes
+
+                 .Select(r => new { Code = r.Id, NombreCompleto = r.Nombre });
+
+
+            foreach (var item in r3a) Console.WriteLine($"{item.Id}");// Id
+            foreach (var item in r3a) Console.WriteLine($"{item.Nombre}");// Nombre
+            foreach (var item in r3c) Console.WriteLine($"{item.Code}");//Con Alias
+
+
+
+            //T-SQL: SELECT Descripcion FROM dbo.ListaOridyctis WHERE precio < 0.90
+
+            // Métodos de LINQ
+            var r4a = DataLists.ListaProductos
+                .Where(r => r.Precio < 0.90)
+                .Select(r => r.Descripcion);//lambda function
+
+            // Expresion de LINQ
+            var r4b = from r in DataLists.ListaProductos
+                      where r.Precio < 0.90
+                      select r;
+
+
+            foreach (var item in r4a) Console.WriteLine($"{item}");
+
+            
+
+            //T-SQL: SELECT Descripcion FROM dbo.ListaOridyctis WHERE precio < 0.90 ORDERBY Description
+            // Métodos de LINQ
+            var r5a = DataLists.ListaProductos
+                .Where(r => r.Precio < 0.90)
+                .OrderByDescending(r => r.Descripcion) 
+                .Select(r => r.Descripcion);//lambda function
+
+            // Expresion de LINQ
+            var r5b = from r in DataLists.ListaProductos
+                      where r.Precio < 0.90
+                      orderby r.Descripcion ascending 
+                      select r.Descripcion;
+
+            Console.WriteLine("");
+            foreach (var item in r5b) Console.WriteLine($"{item}");
+
+            
+
+            //Otros Ejemplos
+
+            // Contains -> Contiene; StarsWith -> Comienza; EndsWith -> Finaliza
+
+            // Métodos de LINQ
+            var r6a = DataLists.ListaProductos
+                .Where(r => r.Descripcion.ToLower().Contains("boli"))
+                .Select(r => r);//lambda function
+
+            // Expresion de LINQ
+            var r6b = from r in DataLists.ListaProductos
+                      where r.Descripcion.ToLower().Contains("boli")
+                      select r;
+
+
+            Console.WriteLine("");
+            foreach (var item in r6b) Console.WriteLine($"{item.Precio}");
+
+            Console.Clear();
+
+            // Count -> Cuenta los elmentos
+            // Distinct -> Valor distinto
+            // Max -> valor máximo;
+            // Min -> valor minimo
+            // Sum -> suma de valores;
+            // Average -> media de los valores
+            // Aggregate -> Aplicar formula o métod de Agregación
+
+
+            var r7a = DataLists.ListaProductos
+                .Where(r => r.Precio < 0.90)
+                .Count();
+
+
+            var r7b = DataLists.ListaProductos
+                .Count(r => r.Precio < 0.90);
+
+
+            var r7c = (from r in DataLists.ListaProductos
+                       where r.Precio < 0.90
+                       select r.Descripcion).Count();
+
+
+        }
+    }
+}
