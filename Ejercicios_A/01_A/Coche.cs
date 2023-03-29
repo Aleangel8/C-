fb4b877adc48a54b17f1311440b6b145a2d5218a
Angel Alejandro Ruiz Centeno <aleangelrc@gmail.com>
2023-03-30 00:35:48 +0200

    Ejercicio 1_A

---
+++ Ejercicios_A/01_A/Coche.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_A
+{
+    internal class Coche
+    {
+        public float Km {get;set;}
+        public float Litros { get;set;}
+
+        public float Consumo() 
+        {
+            return this.Litros / this.Km;
+        }
+    }
+}
---
+++ Ejercicios_A/01_A/Program.cs
@@ -0,0 +1,86 @@
+﻿// ########################################################################################
+// #  Ejercicio 1A
+// ########################################################################################
+// """  Escribe una función para calcular el consumo medio de combustible de un vehículo.
+
+// Entrada de datos:
+//     -Kilometros recorridos
+//     -Listros de combustible consumidos
+// Salida de datos:
+//     -litros consumidos por kilometro
+// """
+// #########################################################################################
+// #Con funcion
+// """def consumo(Km, Litros):
+//     return Litros/Km"""
+
+
+
+
+//Toma de datos
+
+using _01_A;
+using System.ComponentModel.Design;
+
+int volver = 1;
+while (volver == 1)
+{
+    while (volver == 1)
+    {
+        try
+        {
+            var coche = new Coche();
+            Console.WriteLine("Bienvenido");
+            Console.Write("Introduzca la cantidad de Litros: ");
+            coche.Litros = float.Parse(Console.ReadLine());
+            //Console.WriteLine(coche.Litros);
+            Console.Write("Introduzca la cantidad de Km: ");
+            coche.Km = float.Parse(Console.ReadLine());
+            if(coche.Km==0)
+                throw new DivideByZeroException();
+            //Console.WriteLine(coche.Km);
+            //Console.WriteLine(coche.Km.GetType());
+            Console.WriteLine($"El coche consume: {coche.Consumo():N2} Litros por KM");
+            volver = 0;
+
+        }
+
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Datos incorrectos no se puede convertir");
+
+        }
+        catch (System.DivideByZeroException) 
+        {
+            Console.WriteLine("Division por 0 no permitida");
+            Console.ReadKey();
+        }
+    }
+
+    int volver2 = 1;
+    while (volver2 == 1) 
+    {
+        try
+        {
+            Console.WriteLine("Pulse 0 para Salir, Cualquier otro numero volver a intentar : ");
+            volver = int.Parse(Console.ReadLine());
+            if (volver!= 0) 
+            { 
+                volver = 1;
+                volver2 = 0;
+            }
+            else
+            {
+                volver2 = 0;
+            }
+
+
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("No se introdujo un numero");
+            volver2 = 1;
+
+        }    
+    }
+}
